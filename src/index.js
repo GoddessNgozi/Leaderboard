109c470b961b031a759356566f1f478cd39bd189
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-10-11 17:16:43 +0100

    Refactor postScore function

--- src/index.js
+++ src/index.js
@@ -3,15 +3,4 @@ import postScore from './modules/postScore.js';
 
 document.getElementById('refresh').addEventListener('click', getScores);
 
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const name = document.querySelector('#name').value;
-  const score = document.querySelector('#score').value;
-  const data = {
-    user: name.trim(),
-    score: score,
-  };
-  postScore(data);
-  form.reset();
-});
+document.querySelector('form').addEventListener('submit', postScore);
--- src/modules/postScore.js
+++ src/modules/postScore.js
@@ -1,12 +1,20 @@
-const postScore = async (data) => {
-    const post = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/24c0c3c116974ac49488d4eb0267bbc3/scores/', post);
+const postScore = async (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#name').value;
+  const score = document.querySelector('#score').value;
+  const data = {
+    user: name.trim(),
+    score: score,
+  };
+  const post = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/24c0c3c116974ac49488d4eb0267bbc3/scores/', post);
+  document.querySelector('form').reset();
   };
 
   export default postScore;
