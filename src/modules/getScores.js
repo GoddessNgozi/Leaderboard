6a28d6693c5334b1241896e35e384be268c71891
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-10-11 19:09:51 +0100

    Add bootstrap styling

--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -2,11 +2,11 @@ const getScores = async () => {
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/24c0c3c116974ac49488d4eb0267bbc3/scores/');
     const data = await response.json();
     const { result } = data;
-    const container = document.getElementById('container');
-    container.innerHTML = '';
+    const board = document.getElementById('board');
+    board.innerHTML = '';
     result.forEach((item) => {
-        container.innerHTML += `
-        <li>${item.user}: ${item.score}</li>
+        board.innerHTML += `
+        <tr><td>${item.user}: ${item.score}</td></tr>
         `;
     })
 };
