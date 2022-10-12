dd65027a7e182a042e76b19272a568052a866eaa
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-10-12 16:37:23 +0100

    Add function to post score

--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-const result = [
-  {
-    user: 'Ngozi',
-    score: '100',
-  },
-  {
-    user: 'Jenny',
-    score: '500',
-  },
-];
-
-const getScores = () => {
-  const board = document.getElementById('board');
-  board.innerHTML = '';
-  result.forEach((item) => {
-    board.innerHTML += `
-          <tr><td>${item.user}: ${item.score}</td></tr>
-          `;
-  });
-};
-
-const postScore = (e) => {
-  e.preventDefault();
-  const name = document.querySelector('#name').value;
-  const score = document.querySelector('#score').value;
-  const data = {
-    user: name.trim(),
-    score,
-  };
-  result.push(data);
-  document.querySelector('form').reset();
-};
-
-document.getElementById('refresh').addEventListener('click', getScores);
+import postScore from './modules/postscore.js';
 
 document.querySelector('form').addEventListener('submit', postScore);
---
+++ src/postgame.js
@@ -0,0 +1,17 @@
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+postData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { name: 'FunLoop' })
+  .then((data) => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
